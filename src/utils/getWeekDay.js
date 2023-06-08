655c717725f4ed070805430013e8670160415673
AppVeyr <ci@appveyor.com>
2023-06-08 20:19:04 +0300

    fix

--- src/utils/getWeekDay.js
+++
@@ -1,6 +0,0 @@
-export default function getWeekDay() {
-  const days = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
-  const date = new Date();
-
-  return days[date.getDay()];
-}
